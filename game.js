599f3fa08e5ccf0ed1c23e9213e14ffceb656a4f
GGamerE <ue27wnvfmag@hotmail.com>
2025-01-06 14:30:11 +0800

    add player in game

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ class GameScene extends Phaser.Scene {
         this.load.image('sky', 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAiIGhlaWdodD0iNjAiIHZpZXdCb3g9IjAgMCA4MCA2MCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjgwIiBoZWlnaHQ9IjYwIiBmaWxsPSIjODdDRUVCIi8+Cjwvc3ZnPgo=');
         this.load.image('star', 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHN0YXIgY3g9IjEyIiBjeT0iMTIiIHI9IjEwIiBmaWxsPSIjRkZENzAwIi8+Cjwvc3ZnPgo=');
         this.load.image('bomb', 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPGNpcmNsZSBjeD0iMTAiIGN5PSIxMCIgcj0iOCIgZmlsbD0iIzNBM0EzQSIvPgo8L3N2Zz4K');
-        this.load.spritesheet('dude', 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iNDgiIHZpZXdCb3g9IjAgMCAzMiA0OCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjMyIiBoZWlnaHQ9IjQ4IiBmaWxsPSIjRkY2QzM3Ii8+Cjwvc3ZnPgo=', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('player', 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHZpZXdCb3g9IjAgMCAzMiAzMiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHJlY3Qgd2lkdGg9IjMxIiBoZWlnaHQ9IjMxIiB4PSIwLjUiIHk9IjAuNSIgZmlsbD0iI0ZGMDAwMCIgc3Ryb2tlPSIjQ0MwMDAwIiBzdHJva2Utd2lkdGg9IjEiLz48L3N2Zz4=');
         
         this.load.image('tiles', 'assets/tileset.png');
         this.load.tilemapTiledJSON('map', 'assets/tilemap.json');
@@ -24,29 +24,10 @@ class GameScene extends Phaser.Scene {
         platforms.setCollisionByProperty({ collides: true });
         platforms.setCollisionByExclusion([0]);
 
-        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player = this.physics.add.sprite(64, 384, 'player');
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
 
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'dude', frame: 4 }],
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
-        });
 
         this.boxes = this.physics.add.group();
         this.boxes.create(200, 400, 'box');
@@ -92,15 +73,12 @@ class GameScene extends Phaser.Scene {
     update() {
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
-            this.player.anims.play('left', true);
             this.pushBoxes('left');
         } else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160);
-            this.player.anims.play('right', true);
             this.pushBoxes('right');
         } else {
             this.player.setVelocityX(0);
-            this.player.anims.play('turn');
         }
 
         if (this.cursors.up.isDown && this.player.body.touching.down) {
@@ -138,8 +116,7 @@ class GameScene extends Phaser.Scene {
 
     hitBomb(player, bomb) {
         this.physics.pause();
-        player.setTint(0xff0000);
-        player.anims.play('turn');
+        player.setTint(0x888888);
         this.gameOver = true;
     }
 }
